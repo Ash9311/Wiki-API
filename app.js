8fdac33210dad66b23a0091659238c9454335609
@Ash9311 <ashutoshmulki7@gmail.com>
2022-03-17 00:14:40 +0530

    chained route handlers using express

--- app.js
+++ app.js
@@ -21,16 +21,16 @@ const articleSchema = {
 };
 const Article = mongoose.model("Article",articleSchema);
 
-app.get("/articles",function(req,res){
+app.route("/articles").get(function(req,res){
     Article.find(function(err, foundArticles){
         if(!err){
             res.send(foundArticles);
         }
         res.send(err);   
     });
-});
+})
 
-app.post("/articles",function(req,res){
+.post(function(req,res){
     
     const newArticle = new Article({
         title: req.query.title,
@@ -44,9 +44,18 @@ app.post("/articles",function(req,res){
             res.send("chombu");
         }
     });
+})
+
+.delete(function(req,res){
+    Article.deleteMany(function(err){
+        if(!err){
+            res.send("Successfully deleted all articles.");
+        }
+        else{
+            res.send(err);
+        }
+    })
 });
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
