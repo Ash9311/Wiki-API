ed7ff67ed3deaf9e89655d3b4483115f042666cf
@Ash9311 <ashutoshmulki7@gmail.com>
2022-03-18 16:09:05 +0530

    patch,put,delete

--- app.js
+++ app.js
@@ -60,7 +60,9 @@ app.route("/articles").get(function(req,res){
 });
 
 //requests
-app.route("/articles/:articleTitle").get(function(req,res){
+app.route("/articles/:articleTitle")
+
+.get(function(req,res){
     Article.findOne({title: req.params.articleTitle},function(err,foundArticles){
         if(foundArticles){
             res.send(foundArticles);
@@ -69,7 +71,50 @@ app.route("/articles/:articleTitle").get(function(req,res){
             res.send("no articles was found")
         }
     });
+})
+
+.put(function(req,res){
+    Article.update(
+        {title: req.params.articleTitle},
+        {title: req.body.title,content:req.body.content},
+        {overwrite:true},
+        function(err){
+            if(!err){
+                res.send("Successfully updated article.")
+            }
+        }
+    );
+})
+
+.patch(function(req,res){
+   Article.update(
+       {title: req.params.articleTitle},
+       {$set: req.body},
+       function(err){
+        if(!err){
+            res.send("Successfully updated article.")
+        }
+        else{
+            res.send(err);
+        }
+    }
+   );
+})
+
+.delete(function(req,res){
+    Article.deleteOne(
+        {title: req.params.articleTitle},
+        function(err){
+            if(!err){
+            res.send("Successfully deleted article.")
+            }
+            else{
+                res.send(err);
+            }
+        }
+    )
 });
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
